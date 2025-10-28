5fd52122ceb6463d5a70c3f0885c422494a9eb48
Ilya <ilya.shmarov13@mail.ru>
2025-10-28 20:12:22 +0300

    done100

--- LABA3/First/First/obj/Debug/net8.0/First.AssemblyInfo.cs
+++ LABA3/First/First/obj/Debug/net8.0/First.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("First")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+16797bc92169754bdf1061b0a4713d2c88f23e4a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e6556dbf235190809e89db453f5bb7a81b0ce28f")]
 [assembly: System.Reflection.AssemblyProductAttribute("First")]
 [assembly: System.Reflection.AssemblyTitleAttribute("First")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
