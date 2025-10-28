2b5ca435ea9923447da537ea905ce2c6dbf0dcc1
Ilya <ilya.shmarov13@mail.ru>
2025-10-28 16:19:29 +0300

    First

---
+++ LABA1/First/First/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LR_One
+{
+    class First
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Лабораторная работа №1: РАЗРАБОТКА КОНСОЛЬНОГО ПРИЛОЖЕНИЯ");
+            Console.WriteLine("");
+            Console.WriteLine("ФИО: Шмаров Илья Андреевич");
+            Console.WriteLine("Группа: ИДБ-23-01");
+            Console.WriteLine("Дата рождения: 02.08.2005");
+            Console.WriteLine("г.Москва");
+            Console.WriteLine("Программирование");
+            Console.WriteLine("Занимаюсь спортом, хожу на учебу, программирую");
+            Console.WriteLine("");
+            Console.WriteLine("Для завершения работы программы нажмите любую клавишу...");
+            Console.ReadKey();
+        }
+    }
+}
